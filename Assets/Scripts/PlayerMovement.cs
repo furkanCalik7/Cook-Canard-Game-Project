3d00de85937c73866a96803b92a931265ade1d24
Furkan <furkan.calik@ug.bilkent.edu.tr>
2024-09-20 13:22:40 +0200

    photos added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Can different modifiers
+    public static PlayerMovement Instance { get; private set; }
+
+    public event EventHandler<OnSelectedClearCounterChangedEventArgs> OnSelectedClearCounterChanged;
+    public class OnSelectedClearCounterChangedEventArgs : EventArgs
+    {
+        public ClearCounter selectedCounter;
+
+    }
 
     [SerializeField] private float speed = 7f;
     [SerializeField] private float rotationField = 10f;
@@ -12,13 +21,36 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 lastInteractedDir;
     [SerializeField] private LayerMask counterLayerMask;
 
+    private ClearCounter selectedClearCounter;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There is more than one player!!");
+        }
+        Instance = this;
+    }
+
     void Start()
     {
         gameInput.OnInteractAction += GameInput_OnInteractAction;
     }
 
     private void GameInput_OnInteractAction(object sender, EventArgs e)
-    { 
+    {
+        if (selectedClearCounter == null) return;
+        selectedClearCounter.Interact();
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    private void HandleInteractions()
+    {
         float interactionDistance = 6f;
         bool isInteracted = Physics.Raycast(transform.position, lastInteractedDir, out RaycastHit hitInfo, interactionDistance, counterLayerMask);
 
@@ -26,15 +58,22 @@ public class PlayerMovement : MonoBehaviour
         {
             if (hitInfo.transform.TryGetComponent(out ClearCounter clearCounter))
             {
-                clearCounter.Interact();
+                if (selectedClearCounter != clearCounter)
+                {
+                    SetSelectedCounter(clearCounter);
+                }
             }
+            else
+            {
+                SetSelectedCounter(null);
+            }
+        }
+        else
+        {
+            SetSelectedCounter(null);
         }
     }
 
-    void Update()
-    {
-        HandleMovement();
-    }
 
     public bool IsWalking()
     {
@@ -55,12 +94,22 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.position += moveDir * moveDistance;
         }
-        if (moveDir != Vector3.zero) {
+        if (moveDir != Vector3.zero)
+        {
             lastInteractedDir = moveDir;
         }
         isWalking = moveDir != Vector3.zero;
         // Look the direction you go
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationField);
+    }
+    private void SetSelectedCounter(ClearCounter selectedCounter)
+    {
+        Debug.Log("herel");
+        selectedClearCounter = selectedCounter;
+        OnSelectedClearCounterChanged?.Invoke(this, new OnSelectedClearCounterChangedEventArgs
+        {
+            selectedCounter = selectedClearCounter
+        });
     }
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is an exercise project to learn and improve unity.
+Screenshots:
+![Duck and counters](/Photos/screen_1.png)
